37c42de7ae7f9b88bf530eab928fd6c4b862581f
Alexa Kruckenberg <mouldy.k@gmail.com>
2023-09-09 09:47:31 -0700

    test post-push hook

--- src/components/blog/index.js
+++ src/components/blog/index.js
@@ -3,6 +3,5 @@ import styled from "styled-components";
 const Container = styled.div``;
 
 export default function Blog({ testId, children }) {
-  //test
   return <Container testId={testId}>{children}</Container>;
 }
