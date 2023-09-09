3ba82e24a681d29d3cfde879791489033b4323d9
Alexa Kruckenberg <mouldy.k@gmail.com>
2023-09-08 18:19:32 -0700

    test hook

--- src/components/blog/index.js
+++ src/components/blog/index.js
@@ -3,5 +3,6 @@ import styled from "styled-components";
 const Container = styled.div``;
 
 export default function Blog({ testId, children }) {
+  //test
   return <Container testId={testId}>{children}</Container>;
 }
