acdf3e4838f96cdedd488fa6549719006b8b883b
Alexa Kruckenberg <mouldy.k@gmail.com>
2023-09-17 00:50:52 -0700

    more work on fixing scrollbar issue

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -11,7 +11,7 @@ const Container = styled.div`
   padding: 24px;
   height: ${footerHeight}px;
   background: ${colors.darkestBlue};
-  width: 100%
+  margin-right: -50px;
   z-index: 1;
   margin-top: 48px;
 `;
